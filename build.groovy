3fa995a0191d713478dade2f36c9854601cd4ca5
Liora Kamil <liorakamil@Assafs-MacBook-Pro.local>
2020-01-31 11:18:12 +0200

    SCM

--- build.groovy
+++ build.groovy
@@ -1,5 +1,8 @@
 node {
  def customImage = ""
+ stage("pull code") {
+     checkout scm
+ }
  stage("build docker") {
     customImage = docker.build("liorakamil/liorakamil:1.0.1")
     withDockerRegistry(credentialsId: 'eb94b483-01ea-46c4-ad1a-747b29513365') {
