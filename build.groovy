9c401b1f755169e457124b652497ffcbfed37a87
Liora Kamil <liorakamil@Assafs-MacBook-Pro.local>
2020-02-02 20:45:29 +0200

    Parameters

--- build.groovy
+++ build.groovy
@@ -1,23 +1,27 @@
 node {
- def customImage = ""
- stage("pull code") {
-     checkout scm
- }
- stage("build docker") {
-    customImage = docker.build("liorakamil/mid-project")
-    withDockerRegistry(credentialsId: 'dockerhub') {
-        customImage.push()   
+    parameters {
+        string(name: 'EKS', description: 'EKS cluster name')
     }
- }
- stage("verify dockers") {
-  sh "docker images"
- }
- stage('Apply Kubernetes files') {
-    withAWS(region: 'us-east-1', credentials: 'AWSK8S') {
-        sh """
-        aws eks update-kubeconfig --name opsSchool-eks-ENrA99HA
-        kubectl apply -f deploy.yml
-        """
+    def customImage = ""
+    stage("pull code") {
+        checkout scm
     }
-  }
+    
+    stage("build docker") {
+        customImage = docker.build("liorakamil/mid-project")
+        withDockerRegistry(credentialsId: 'dockerhub') {
+            customImage.push()   
+        }
+    }
+    stage("verify dockers") {
+        sh "docker images"
+    }
+    stage('Apply Kubernetes files') {
+        withAWS(region: 'us-east-1', credentials: 'AWSK8S') {
+            sh """
+            aws eks update-kubeconfig --name ${params.EKS}
+            kubectl apply -f deploy.yml
+            """
+        }
+    }
 }
