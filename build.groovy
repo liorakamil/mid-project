f4254d4d2d63fb92b38c20fc130c69dbcf9fa78d
Liora Kamil <liorakamil@Assafs-MacBook-Pro.local>
2020-02-02 22:52:23 +0200

    fixed cluster name

--- build.groovy
+++ build.groovy
@@ -1,29 +1,25 @@
 node {
-    properties([
-        parameters ([
-            string(name: 'EKS', description: 'EKS cluster name')
-        ])
-    ])
-    def customImage = ""
-    stage("pull code") {
-        checkout scm
+ def customImage = ""
+ stage("pull code") {
+     checkout scm
+ }
+
+ stage("build docker") {
+    customImage = docker.build("liorakamil/mid-project")
+    withDockerRegistry(credentialsId: 'dockerhub') {
+        customImage.push()   
     }
-    
-    stage("build docker") {
-        customImage = docker.build("liorakamil/mid-project")
-        withDockerRegistry(credentialsId: 'dockerhub') {
-            customImage.push()   
-        }
+ }
+
+ stage("verify dockers") {
+  sh "docker images"
+ }
+ stage('Apply Kubernetes files') {
+    withAWS(region: 'us-east-1', credentials: 'AWSK8S') {
+        sh """
+        aws eks update-kubeconfig --name eks-cluster-flask
+        kubectl apply -f deploy.yml
+        """
     }
-    stage("verify dockers") {
-        sh "docker images"
-    }
-    stage('Apply Kubernetes files') {
-        withAWS(region: 'us-east-1', credentials: 'AWSK8S') {
-            sh """
-            aws eks update-kubeconfig --name ${params.EKS}
-            kubectl apply -f deploy.yml
-            """
-        }
-    }
+  }
 }
