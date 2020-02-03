05413666d1f7b2fbc6b5ff608d877ce0747e7f88
Liora Kamil <liorakamil@Assafs-MacBook-Pro.local>
2020-02-03 21:38:03 +0200

    Verify docker

--- build.groovy
+++ build.groovy
@@ -13,8 +13,8 @@ node {
 
  stage("verify docker") {
   sh """
-  docker_id=$(docker run -d -p 5000:5000 liorakamil/mid-project:${env.BUILD_ID})
-  status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:5000)
+  docker_id=\$(docker run -d -p 5000:5000 liorakamil/mid-project:${env.BUILD_ID})
+  status_code=\$(curl --write-out %{http_code} --silent --output /dev/null localhost:5000)
   if [[ "\$status_code" -ne 200 ]] ; then
     echo "Docker failed with \$status_code"
     exit 1
