9de65b0182c427565e0ebd34db853d086fbd2810
Liora Kamil <liorakamil@Assafs-MacBook-Pro.local>
2020-02-03 20:01:38 +0200

    Deploy with build id docker

--- build.groovy
+++ build.groovy
@@ -20,40 +20,40 @@ node {
         aws eks update-kubeconfig --name eks-cluster-flask
         
         cat <<EOF | kubectl apply -f -
-        apiVersion: v1
-        kind: Service
-        metadata:
-        name: flask-service
-        labels:
-            app: flask
-        spec:
-        type: LoadBalancer
+apiVersion: v1      # for versions before 1.9.0 use apps/v1beta2
+kind: Service
+metadata:
+  name: flask-service
+  labels:
+    app: flask
+spec:
+  type: LoadBalancer
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 5000
+  selector:
+    app: flask
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: flask-deployment
+spec:
+  selector:
+    matchLabels:
+      app: flask
+  replicas: 2 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: flask
+    spec:
+      containers:
+      - name: flask
+        image: liorakamil/mid-project:${env.BUILD_ID}
         ports:
-        -   protocol: TCP
-            port: 80
-            targetPort: 5000
-        selector:
-            app: flask
-        ---
-        apiVersion: apps/v1
-        kind: Deployment
-        metadata:
-        name: flask-deployment
-        spec:
-        selector:
-            matchLabels:
-            app: flask
-        replicas: 2
-        template:
-            metadata:
-            labels:
-                app: flask
-            spec:
-            containers:
-            - name: flask
-                image: liorakamil/mid-project:${env.BUILD_ID}
-                ports:
-                - containerPort: 5000        
+        - containerPort: 5000       
         EOF
         """
     }
